b5ea992d422be69da0ce49c3f43d3d9bb1d7bcd5
Edgar Munoz <edgmuo1085@gmail.com>
2019-01-22 01:31:52 -0500

    Se agrego los nuevos estilos que identifican al chat, material-kit.css, se agrego un nuevo index como portada inicial y se cambiaron las rutas y agregaron otras

--- server/router.js
+++ server/router.js
@@ -10,7 +10,7 @@ const jwt = require('jsonwebtoken');
 
 function checkAuth (req, res, next) {
     passport.authenticate('jwt', { session: false }, (err, decryptToken, jwtError) => {
-        if(jwtError != void(0) || err != void(0)) return res.render('index.html', { error: err || jwtError});
+        if(jwtError != void(0) || err != void(0)) return res.render('chat.html', { error: err || jwtError});
         req.user = decryptToken;
         next();
     })(req, res, next);
@@ -27,8 +27,12 @@ function createToken (body) {
 module.exports = app => {
     app.use('/assets', express.static('./client/public'));
 
-    app.get('/', checkAuth, (req, res) => {
-        res.render('index.html', { username: req.user.username });
+    app.get('/', (req, res) => {
+        res.render('home.html');
+    });
+
+    app.get('/chat', checkAuth, (req, res) => {
+        res.render('chat.html', { username: req.user.username });
     });
 
     app.post('/login', async (req, res) => {
